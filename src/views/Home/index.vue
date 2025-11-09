c49c0020f98e531d69d0eeabbce315abb1dec72f
黄坤蓝 <huang2722348243@hotmail.com>
2025-11-09 17:00:51 +0800

    home-整体结构拆分和数据渲染

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,13 +1,26 @@
+<script setup>
+import HomeBanner from './components/HomeBanner.vue'
+import HomeCategory from './components/HomeCategory.vue'
+import HomeNew from './components/HomeNew.vue'
+import HomeHot from './components/HomeHot.vue'
+import HomeProduct from './components/HomeProduct.vue'
+
+
+
+</script>
+
+
 <template>
-    <div>
-        <h1>Home</h1>
+    <div class="container">
+        <HomeCategory />
+        <HomeBanner />
     </div>
+    
+    <HomeNew />
+    <HomeHot />
+    <HomeProduct />
 </template>
 
 <style scoped lang="scss">
-div {
-  height: 1200px;
-}
-
 
 </style>
